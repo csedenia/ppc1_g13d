d1c3f5ede95d5889529195f2c680ce1bc2403de9
fyl <ylfungad@connect.ust.hk>
2023-04-17 15:42:55 +0800

    updated data members, constructors for FunctionB, FunctionC

--- src/main/java/function_c/FunctionC.java
+++ src/main/java/function_c/FunctionC.java
@@ -3,14 +3,22 @@ package function_c;
 import function_b.FunctionB;
 
 public class FunctionC extends FunctionB{
-	//data member
+	//data members
 	private int Bko_Rose;
 	private int Bko_Noir;
+	
+	//data member for output
+	private boolean Bko_fulfill = false;
 	
 	//constructor
 	public FunctionC(int WeekOfYear, int Cap_Labor, int Cap_Grape, float Prc_Rose, float Prc_Noir, int Bko_Rose, int Bko_Noir){
 		super(WeekOfYear, Cap_Labor, Cap_Grape, Prc_Rose, Prc_Noir);
 		this.Bko_Rose = Bko_Rose;
 		this.Bko_Noir = Bko_Noir;
+	}
+	
+	//reserve Cap_Labor and Cap_Grape to backorder before calculating product mix
+	public void reserve_backorder() {
+		return;
 	}
 }
