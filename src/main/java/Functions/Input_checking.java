8ff2497c03182d34f89c53a8fe001c8322f6c612
Hardy0611 <hardy110602@gmail.com>
2023-04-19 00:30:52 +0800

    made the input checking for all input parameter in functionB

--- src/main/java/Functions/Input_checking.java
+++ src/main/java/Functions/Input_checking.java
@@ -18,4 +18,48 @@ public class Input_checking {
 		}
 	}
 	
+	public static boolean labor_capacity_checking(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		}
+		catch(NumberFormatException e){
+			System.out.println("Input is not a valid integer.");
+			return false;
+		}
+	}
+	
+	public static boolean grape_capacity_checking(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		}
+		catch(NumberFormatException e){
+			System.out.println("Input is not a valid integer.");
+			return false;
+		}
+	}
+	
+	public static boolean pr_checking(String input) {
+		try {
+			Float.parseFloat(input);
+			return true;
+		}
+		catch(NumberFormatException e){
+			System.out.println("Input is not a valid float.");
+			return false;
+		}
+	}
+	
+	public static boolean pn_checking(String input) {
+		try {
+			Float.parseFloat(input);
+			return true;
+		}
+		catch(NumberFormatException e){
+			System.out.println("Input is not a valid float.");
+			return false;
+		}
+	}
+	
 }
