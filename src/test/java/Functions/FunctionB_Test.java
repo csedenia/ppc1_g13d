3eec982e2a62fad893937c91062b5fcfb924c6d0
Hardy0611 <hardy110602@gmail.com>
2023-04-20 15:55:35 +0800

    made two boundary testing for Opt_revenue

--- src/test/java/Functions/FunctionB_Test.java
+++ src/test/java/Functions/FunctionB_Test.java
@@ -4,11 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class FunctionB_Test {
 
+public class FunctionB_Test {
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() { //boundary testing 1 for update()
+		FunctionB tester = new FunctionB(2308, 400, 300, 0f, 14.9f); // Initialize the input data, assume they are all correct
+		tester.update();
+		assertEquals(tester.getOpt_Revenue(), 491);
+	}
+	@Test
+	public void test2() { //boundary testing 2 for update()
+		FunctionB tester = new FunctionB(2308, 400, 300, 15.9f, 0f); // Initialize the input data, assume they are all correct
+		assertEquals(tester.getOpt_Revenue(), 795);
 	}
 
 }
