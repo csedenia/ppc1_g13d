25af187d3c260afc701e91578d317e7402e6b171
Hardy0611 <hardy110602@gmail.com>
2023-04-20 18:01:07 +0800

    made typical values test for update()

--- src/test/java/Functions/FunctionB_Test.java
+++ src/test/java/Functions/FunctionB_Test.java
@@ -11,12 +11,27 @@ public class FunctionB_Test {
 	public void test1() { //boundary testing 1 for update()
 		FunctionB tester = new FunctionB(2308, 400, 300, 0f, 14.9f); // Initialize the input data, assume they are all correct
 		tester.update();
-		assertEquals(tester.getOpt_Revenue(), 491);
+		assertEquals(491, tester.getOpt_Revenue());
 	}
 	@Test
 	public void test2() { //boundary testing 2 for update()
 		FunctionB tester = new FunctionB(2308, 400, 300, 15.9f, 0f); // Initialize the input data, assume they are all correct
-		assertEquals(tester.getOpt_Revenue(), 795);
+		tester.update();
+		assertEquals(795, tester.getOpt_Revenue());
 	}
+	@Test
+	public void test3() { //typical values testing 1 for update()
+		FunctionB tester = new FunctionB(2308, 100, 100, 1f, 2f); // Initialize the input data, assume they are all correct
+		tester.update();
+		assertEquals(19, tester.getOpt_Revenue());
+	}
+	@Test
+	public void test4() { //typical values testing 1 for update()
+		FunctionB tester = new FunctionB(2308, 200, 200, 3f, 5f); // Initialize the input data, assume they are all correct
+		tester.update();
+		assertEquals(110, tester.getOpt_Revenue());
+	}
+	
+	
 
 }
