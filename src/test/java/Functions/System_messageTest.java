9d273fde3d90e87efdc2e14e0ddd0abb9cf30607
fyl <ylfungad@connect.ust.hk>
2023-05-05 18:52:43 +0800

    changed names for testing functions in System_messageTest.java

--- src/test/java/Functions/System_messageTest.java
+++ src/test/java/Functions/System_messageTest.java
@@ -7,15 +7,16 @@ import org.junit.Test;
 public class System_messageTest {
 
 	@Test
-	public void A01_calculateGrossProfit() {
+	public void W02_SystemMessage() {
 		FunctionA aTest = new FunctionA(1,1000,50000,12.5f,23.5f,112800);
 		aTest.calculateGrossProfit();
 		System_message output = new System_message(aTest.getOptimalRose(), aTest.getOptimalNoir(), 1, 120000, 1000);
+		System.out.println(output);
 		assertEquals(output, 2);
 	}
 
 	@Test
-	public void A01_calculateGrossProfit() {
+	public void W00_SystemMessage() {
 		FunctionA aTest = new FunctionA(1,120000,50000,12.5f,23.5f,112800);
 		aTest.calculateGrossProfit();
 		System_message output = new System_message(aTest.getOptimalRose(), aTest.getOptimalNoir(), 1, 120000, 1000);
