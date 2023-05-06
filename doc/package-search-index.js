8b98c34d1218d2363e3a72ea69a5b6c596b5bbe3
fyl <ylfungad@connect.ust.hk>
2023-05-06 15:53:12 +0800

    updated javadoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Functions"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Functions"},{"l":"src.main.java"}];updateSearchResults();
